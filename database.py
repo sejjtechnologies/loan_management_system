7ac64d8e741f11ba6f6e3d091c4adb100ee39456
Sejjusa <sejjtechnologies@gmail.com>
2025-11-12 12:53:30 +0300

    Add Procfile for Render

--- database.py
+++ database.py
@@ -1,18 +1,21 @@
 # database.py
 
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# PostgreSQL credentials
-DB_USER = "postgres"
-DB_PASSWORD = "sejjtechnologies"
-DB_HOST = "localhost"
-DB_PORT = "5432"
-DB_NAME = "moneylend_db"
+# Check for environment variable (used in Render)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Connection URL
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# If not set, use local PostgreSQL credentials
+if not DATABASE_URL:
+    DB_USER = "postgres"
+    DB_PASSWORD = "sejjtechnologies"
+    DB_HOST = "localhost"
+    DB_PORT = "5432"
+    DB_NAME = "moneylend_db"
+    DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # SQLAlchemy engine and session
 engine = create_engine(DATABASE_URL, echo=True)
